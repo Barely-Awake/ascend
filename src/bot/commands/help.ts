b833b2e2408fbc728a456acec479366902936372
Barely-Awake <ryanasperger@gmail.com>
2022-05-25 10:43:59 -0700

    Fixed help.ts using the wrong directory

--- src/bot/commands/help.ts
+++ src/bot/commands/help.ts
@@ -17,7 +17,7 @@ export default async function (message: Message, args: string[]) {
   let totalCommands = 0;
 
   async function fileLoop(pathAdditions: string = '') {
-    const files = readdirSync('./dist/commands' + pathAdditions);
+    const files = readdirSync('./dist/bot/commands' + pathAdditions);
 
     for (const file of files) {
       if (file.startsWith('_') || (file.includes('.') && !file.endsWith('.js')))
