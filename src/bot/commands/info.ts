57dc4ecb983b77517cd5a52fd1a7a6e65bef9f3a
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-05-25 21:42:52 -0700

    Added information on current host to info.ts

--- src/bot/commands/info.ts
+++ src/bot/commands/info.ts
@@ -1,8 +1,18 @@
-import { Message, MessageEmbed } from 'discord.js';
+import { Message, MessageEmbed, User, Team } from 'discord.js';
 import { DescriptionTypes } from './_example.js';
 import config from '../../utils/readConfig.js';
 
 export default async function (message: Message, args: string[]) {
+  let clientApplication = await message.client.application?.fetch();
+  let applicationOwner;
+
+  if (clientApplication?.owner instanceof User)
+    applicationOwner = (clientApplication?.owner || 'Unknown').toString();
+  else if (clientApplication?.owner instanceof Team)
+    applicationOwner = (clientApplication?.owner.owner || 'Unknown').toString();
+  else
+    applicationOwner = 'Unknown';
+
   const infoEmbed = new MessageEmbed()
     .setTitle(`Information on ${config.botName}`)
     .setColor(config.colors[1])
@@ -15,7 +25,7 @@ export default async function (message: Message, args: string[]) {
       `The source code can be found at https://github.com/Barely-Awake/raging-bisexual. ` +
       `Currently used in over ${message.client.guilds.cache.size} guilds with ${message.client.users.cache.size} ` +
       `members, ${config.botName} features a lot of random commands. Currently there isn't a real direction besides ` +
-      `just being a useful bot.`,
+      `just being a useful bot.\n\nThis instance of the bot is being hosted by ${applicationOwner}`,
     )
     .setFooter({
       text: 'Made with love & discord.js',
