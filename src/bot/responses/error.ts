0193e312718dad60921b8aec11c471fecd2f5c64
Barely-Awake <ryanasperger@gmail.com>
2022-05-26 14:30:11 -0700

    Added error response

---
+++ src/bot/responses/error.ts
@@ -0,0 +1,15 @@
+import { Message, MessageEmbed } from 'discord.js';
+import config from '../../utils/readConfig.js';
+
+export default function error(error: string, commandName: string, message: Message) {
+  const embed = new MessageEmbed()
+    .setTitle(`Error with command \`${commandName}\``)
+    .setColor('#ff0000')
+    .setDescription(error)
+    .setFooter({
+      text: `Sent by ${config.botName}`,
+      iconURL: config.footerIcon,
+    });
+
+  return message.channel.send({embeds: [embed]});
+}
