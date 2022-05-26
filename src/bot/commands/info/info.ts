c99b2091db8a104577d5ea0d0633047262bd254b
Barely-Awake <ryanasperger@gmail.com>
2022-05-26 14:31:44 -0700

    Moved info.ts into info commands folder

--- src/bot/commands/info/info.ts
+++ src/bot/commands/info/info.ts
@@ -1,6 +1,6 @@
 import { Message, MessageEmbed } from 'discord.js';
-import { DescriptionTypes } from './_example.js';
-import config from '../../utils/readConfig.js';
+import { DescriptionTypes } from '../_example.js';
+import config from '../../../utils/readConfig.js';
 
 export default async function (message: Message, args: string[]) {
   const infoEmbed = new MessageEmbed()
@@ -19,7 +19,7 @@ export default async function (message: Message, args: string[]) {
     )
     .setFooter({
       text: 'Made with love & discord.js',
-      iconURL: 'https://cdn.discordapp.com/emojis/979073032549171200.png?size=240&quality=lossless',
+      iconURL: config.footerIcon,
     });
 
   message.channel.send({embeds: [infoEmbed]});
