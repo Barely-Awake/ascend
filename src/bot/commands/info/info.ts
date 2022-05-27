1d8ee32cb7c10bc68c38075a94a5b0c2b871c758
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-05-26 23:08:07 -0700

    Added more info to info.ts command

--- src/bot/commands/info/info.ts
+++ src/bot/commands/info/info.ts
@@ -1,6 +1,8 @@
-import { Message, MessageEmbed, User, Team } from 'discord.js';
+import { Message, MessageEmbed, Team, User, version } from 'discord.js';
 import { DescriptionTypes } from '../_example.js';
 import config from '../../../utils/readConfig.js';
+import unixToSeconds from '../../../utils/misc/unixToSeconds.js';
+import messageTimeStamp from '../../../utils/discord/messageTimeStamp.js';
 
 export default async function (message: Message, args: string[]) {
   let clientApplication = await message.client.application?.fetch();
@@ -13,6 +15,12 @@ export default async function (message: Message, args: string[]) {
   else
     applicationOwner = 'Unknown';
 
+  let clientUptime;
+  if (message.client.uptime !== null)
+    clientUptime = unixToSeconds((+new Date()) - message.client.uptime);
+  else
+    clientUptime = 'Unknown';
+
   const infoEmbed = new MessageEmbed()
     .setTitle(`Information on ${config.botName}`)
     .setColor(config.colors[1])
@@ -23,10 +31,19 @@ export default async function (message: Message, args: string[]) {
     })
     .setDescription(`${config.botName} is an open source bot made by Barely Awake. ` +
       `The source code can be found at https://github.com/Barely-Awake/raging-bisexual. ` +
-      `Currently used in over ${message.client.guilds.cache.size} guilds with ${message.client.users.cache.size} ` +
-      `members, ${config.botName} features a lot of random commands. Currently there isn't a real direction besides ` +
-      `just being a useful bot.\n\nThis instance of the bot is being hosted by ${applicationOwner}`,
+      `${config.botName} features a lot of random commands. Currently there isn't a real direction besides just ` +
+      `being a useful bot.`,
     )
+    .addField(`${config.botName} Version`, `\`${process.env.npm_package_version}\`` || '`Unknown`')
+    .addField('Ping', `\`${message.client.ws.ping}\` ms`)
+    .addField('Up Since', typeof clientUptime !== 'string' ?
+      messageTimeStamp(clientUptime, 'R') :
+      'Unknown',
+    )
+    .addField('Instance Host', applicationOwner)
+    .addField('Server Count', String(message.client.guilds.cache.size))
+    .addField('User Count', String(message.client.users.cache.size))
+    .addField('Discord.js Version', version)
     .setFooter({
       text: 'Made with love & discord.js',
       iconURL: config.footerIcon,
