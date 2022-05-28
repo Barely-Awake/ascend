a29c572d3f681ccd944b1543b385a41a26888c48
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-05-27 23:31:07 -0700

    feat(bot): Added getTargetUser.ts

---
+++ src/utils/discord/getTargetUser.ts
@@ -0,0 +1,22 @@
+import { Message } from 'discord.js';
+
+export default async function getTargetUser(message: Message, argument: string) {
+  let user;
+
+  try {
+    if (!argument)
+      user = message.author;
+    else if (message.mentions.users.first())
+      user = message.mentions.users.first();
+    else
+      // If there is no mentions then fetches user from id
+      user = await message.client.users.fetch(argument);
+  } catch {
+    return false;
+  }
+
+  if (user === undefined)
+    user = message.author;
+
+  return user;
+}
