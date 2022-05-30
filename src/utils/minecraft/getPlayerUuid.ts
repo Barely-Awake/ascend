650f5c1e6c2090d83878659b33fabccb2c84a39c
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-05-30 00:26:18 -0700

    change(getPlayerUuid): Made getPlayerUuid.ts return all data given by mojang

--- src/utils/minecraft/getPlayerUuid.ts
+++ src/utils/minecraft/getPlayerUuid.ts
@@ -1,11 +1,12 @@
 import fetch from 'node-fetch';
+import PlayerUuid from '../../types/mojangApi/playerUuid.js';
 
 export default async function (playerName: string) {
   try {
-    let data: any = await fetch(`https://api.mojang.com/users/profiles/minecraft/${playerName}`);
-    data = await data.json();
+    let response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${playerName}`);
+    let data: PlayerUuid = await response.json();
 
-    return data.id;
+    return data;
   } catch (error) {
     return false;
   }
