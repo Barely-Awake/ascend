c2870ae51bae856329e2e56fab8f04c199726b66
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-06-01 15:42:43 -0700

    feat(bot): Added bot presence

--- src/bot/events/ready.ts
+++ src/bot/events/ready.ts
@@ -2,6 +2,15 @@ import { Client } from 'discord.js';
 
 export default async function (client: Client) {
   console.log(`Logged in as ${client.user?.tag}`);
+  client.user?.setStatus('idle');
+  client.user?.setPresence({
+    activities: [
+      {
+        name: 'Happy pride month!',
+        type: 'PLAYING',
+      },
+    ],
+  });
 }
 
 export const settings = {
