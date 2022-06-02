cabaeb57ef9491367639031d505f6610d0a699d1
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-06-01 22:51:00 -0700

    change(bot): Moved bot presence to startup options

--- src/bot/events/ready.ts
+++ src/bot/events/ready.ts
@@ -2,15 +2,6 @@ import { Client } from 'discord.js';
 
 export default async function (client: Client) {
   console.log(`Logged in as ${client.user?.tag}`);
-  client.user?.setStatus('idle');
-  client.user?.setPresence({
-    activities: [
-      {
-        name: 'Happy pride month!',
-        type: 'PLAYING',
-      },
-    ],
-  });
 }
 
 export const settings = {
