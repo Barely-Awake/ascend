3381ff39f936fa6d12c7a4f644d74557393de5e8
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-06-01 23:05:00 -0700

    fix(*): Removed unnecessary async type from functions

--- src/bot/commands/info/invite.ts
+++ src/bot/commands/info/invite.ts
@@ -2,7 +2,7 @@ import { Message, MessageEmbed } from 'discord.js';
 import { DescriptionTypes } from '../_example.js';
 import config from '../../../utils/readConfig.js';
 
-export default async function (message: Message, args: string[]) {
+export default function (message: Message, args: string[]) {
   const embed = new MessageEmbed()
     .setTitle(`Invite for \`${config.botName}\``)
     .setURL(`https://discord.com/api/oauth2/authorize?client_id=${message.client.user?.id}&permissions=412317248576&scope=bot%20applications.commands`);
