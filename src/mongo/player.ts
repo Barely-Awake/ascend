c917e41a28254e201f7b7c9039771304465c1d02
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-06-11 19:45:09 -0700

    change(mongo): player.ts now stores playerName

--- src/mongo/player.ts
+++ src/mongo/player.ts
@@ -5,11 +5,13 @@ const {model, Schema} = pkg;
 interface IPlayer {
   discordId: string;
   playerUuid: string;
+  playerName: string;
 }
 
 const PlayerSchema = new Schema<IPlayer>({
   discordId: String,
   playerUuid: String,
+  playerName: String,
 }, {timestamps: true});
 
 const Player = model<IPlayer>('Player', PlayerSchema);
