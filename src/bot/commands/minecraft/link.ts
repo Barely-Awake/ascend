546d59244d9fb94fb08d1abe1a791c13cd2857a8
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-06-11 18:04:35 -0700

    feat(commands): link.ts now checks discord with discord linked on hypixel

--- src/bot/commands/minecraft/link.ts
+++ src/bot/commands/minecraft/link.ts
@@ -2,7 +2,9 @@ import { Message } from 'discord.js';
 import { DescriptionTypes } from '../_example.js';
 import getPlayerUuid from '../../../utils/minecraft/getPlayerUuid.js';
 import error from '../../responses/error.js';
-import playerModel from '../../../mongo/player.js';
+import Player from '../../../mongo/player.js';
+import getPlayerStats from '../../../utils/minecraft/getPlayerStats.js';
+import formatPlayerStats from '../../../utils/minecraft/formatPlayerStats.js';
 
 export default async function (message: Message, args: string[]) {
   if (!args[0])
@@ -14,12 +16,36 @@ export default async function (message: Message, args: string[]) {
 
   if (args[0].length !== 32)
     playerUuid = await getPlayerUuid(args[0]);
+  else
+    playerUuid = {id: args[0]};
 
-  await playerModel.updateOne({}, {$set: {uuid: playerUuid, discordId: message.author.id}});
+  if (typeof playerUuid === 'boolean')
+    return error('Couldn\'t fetch that uuid', description.name, message);
+
+  playerUuid = playerUuid.id;
+
+  const hypixelData = await getPlayerStats(playerUuid);
+
+  if (typeof hypixelData === 'boolean')
+    return error('Couldn\'t fetch that player\'s Hypixel stats', description.name, message);
+
+  const hypixelStats = formatPlayerStats(hypixelData);
+
+  if (hypixelStats.socialMedia.discord === null)
+    return error('That account doesn\'t have a discord linked to it. If that is your account, you can click ' +
+      'the link below to see how to do that.\nhttps://catboymaid.club/Z96boeByYUZd', description.name, message);
+
+  if (hypixelStats.socialMedia.discord !== message.author.tag)
+    return error('Your discord doesn\'t match with the linked discord on that account ' +
+      `(\`${hypixelStats.socialMedia.discord}\`)`, description.name, message);
+
+  const playerData = new Player({playerUuid: playerUuid, discordId: message.author.id});
+  await playerData.save();
 }
 
 export const description: DescriptionTypes = {
   name: 'link',
-  description: 'Links your discord account to your minecraft account',
+  description: 'Links your discord account to your minecraft account. Allows you to use stat checking commands ' +
+    'without inputting your username',
   usage: '<player>',
 };
