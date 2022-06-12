b0d3c5c065ac84564d38b7856189ce5a2e55b02a
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-06-11 22:07:36 -0700

    feat(utils): Added keathiz winstreak estimates

---
+++ src/utils/minecraft/getWinStreakEstimates.ts
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+import config from '../readConfig.js';
+import keathizApiResponse from '../../types/keathizResponseTypes.js';
+
+export default async function (playerUuid: string) {
+  try {
+    let response = await fetch(
+      `https://api.antisniper.net/winstreak?key=${config.keathizApiKey}&uuid=${playerUuid}`,
+    );
+
+    if (!response.ok)
+      return false;
+
+    const data: keathizApiResponse = await response.json();
+
+    if (!data.success || data.player === undefined || data.player === null)
+      return false;
+
+    let winStreakData = data.player.data;
+
+    return {
+        overAll: winStreakData.overall_winstreak || 0,
+        eight_one: winStreakData.eight_one_winstreak || 0,
+        eight_two: winStreakData.eight_two_winstreak || 0,
+        four_three: winStreakData.four_three_winstreak || 0,
+        four_four: winStreakData.four_four_winstreak || 0,
+        two_four: winStreakData.two_four_winstreak || 0,
+    };
+  } catch {
+    return false;
+  }
+}
