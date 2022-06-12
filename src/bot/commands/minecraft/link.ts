3a7fe1d74e32ab99e9ceea8c510c68f9a9faeef6
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-06-11 19:44:47 -0700

    fix(commands): link.ts now checks to see if discord or minecraft account has already been linked

--- src/bot/commands/minecraft/link.ts
+++ src/bot/commands/minecraft/link.ts
@@ -5,24 +5,47 @@ import error from '../../responses/error.js';
 import Player from '../../../mongo/player.js';
 import getPlayerStats from '../../../utils/minecraft/getPlayerStats.js';
 import formatPlayerStats from '../../../utils/minecraft/formatPlayerStats.js';
+import getPlayerNames from '../../../utils/minecraft/getPlayerNames.js';
 
 export default async function (message: Message, args: string[]) {
+  const existingDiscordData = await Player.find({discordId: message.author.id});
+
+  if (existingDiscordData !== null)
+    return error('You have already linked on this discord account. Please use the unlink command and then try ' +
+      'linking again.', description.name, message);
+
   if (!args[0])
     return error('Please provide the account you want to link with', description.name, message);
 
   args[0] = (args[0] || '').replace(/-/g, '');
 
+  let mojangData;
   let playerUuid;
 
-  if (args[0].length !== 32)
-    playerUuid = await getPlayerUuid(args[0]);
-  else
-    playerUuid = {id: args[0]};
+  if (args[0].length !== 32) {
+    mojangData = await getPlayerUuid(args[0]);
 
-  if (typeof playerUuid === 'boolean')
-    return error('Couldn\'t fetch that uuid', description.name, message);
+    if (typeof mojangData === 'boolean')
+      return error('Couldn\'t fetch that uuid', description.name, message);
 
-  playerUuid = playerUuid.id;
+    playerUuid = mojangData.id;
+  } else {
+    playerUuid = args[0];
+    let nameHistory = await getPlayerNames(playerUuid);
+
+    if (typeof nameHistory === 'boolean')
+      return error('Couldn\'t fetch that players name', description.name, message);
+
+    mojangData = {
+      id: playerUuid,
+      name: nameHistory[nameHistory.length - 1].name,
+    };
+  }
+  const existingMinecraftData = await Player.findOne({playerUuid: playerUuid});
+
+  if (existingMinecraftData !== null)
+    return error(`That minecraft account has already been linked to by <@${existingMinecraftData.discordId}>. ` +
+      `Please use the unlink command on that account and then try linking again.`, description.name, message);
 
   const hypixelData = await getPlayerStats(playerUuid);
 
@@ -36,11 +59,17 @@ export default async function (message: Message, args: string[]) {
       'the link below to see how to do that.\nhttps://catboymaid.club/Z96boeByYUZd', description.name, message);
 
   if (hypixelStats.socialMedia.discord !== message.author.tag)
-    return error('Your discord doesn\'t match with the linked discord on that account ' +
-      `(\`${hypixelStats.socialMedia.discord}\`)`, description.name, message);
+    return error(`Your discord (\`${message.author.tag}\`)doesn't match with the linked discord on that ` +
+      `account (\`${hypixelStats.socialMedia.discord}\`)`, description.name, message);
 
-  const playerData = new Player({playerUuid: playerUuid, discordId: message.author.id});
+  const playerData = new Player({
+    playerUuid: playerUuid,
+    discordId: message.author.id,
+    playerName: mojangData.name,
+  });
   await playerData.save();
+
+  return;
 }
 
 export const description: DescriptionTypes = {
