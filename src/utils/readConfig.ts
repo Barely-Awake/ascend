f5d03d605a206f4900657503c7d2d1d791940754
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-06-12 00:03:17 -0700

    feat(config): Added supportServerId

--- src/utils/readConfig.ts
+++ src/utils/readConfig.ts
@@ -3,24 +3,34 @@ import 'dotenv/config';
 export function readConfig(): Config {
   return <Config>{
     token: process.env['TOKEN'],
+
     betaToken: process.env['BETA_TOKEN'],
     betaMode: process.env['BETA_MODE'] === 'true',
+
     prefix: process.env['PREFIX'],
     botName: process.env['BOT_NAME'],
+    supportServerId: process.env['SUPPORT_SERVER_ID'],
+
     hypixelApiKey: process.env['HYPIXEL_API_KEY'],
     keathizApiKey: process.env['KEATHIZ_API_KEY'],
+
     mongoUrl: process.env['MONGO_URL'],
   };
 }
 
 interface Config {
   token: string;
+
   betaToken: string;
   betaMode: boolean;
+
   prefix: string;
   botName: string;
+  supportServerId: string;
+
   hypixelApiKey: string;
   keathizApiKey: string;
+
   mongoUrl: string;
 }
 
