8c386eb169e306abc24ac29390046900249cda91
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-06-13 17:29:47 -0700

    fix(utils): Made fillColoredText.ts only use one for loop instead of two

--- src/utils/canvas/fillColoredText.ts
+++ src/utils/canvas/fillColoredText.ts
@@ -1,31 +1,26 @@
 import { CanvasRenderingContext2D } from 'canvas';
 import { colors } from '../minecraft/getFormattedLevel.js';
+import removeColorCodes from './removeColorCodes.js';
 
-export default function (stringWithColorCodes: string, ctx: CanvasRenderingContext2D, textPosX: number, textPosY: number, textAlign = 'center'): void {
-  const levelArray = ('§r' + stringWithColorCodes).split(/§/g);
-  let textContent = '';
-  const orderedColors: (string | number)[] = [];
+export default function (text: string, ctx: CanvasRenderingContext2D, textPosX: number, textPosY: number, textAlign = 'center'): void {
+  const levelArray = ('§r' + text).split(/§/g);
 
-  for (let i = 0; i < levelArray.length; i++) {
-    orderedColors.push(levelArray[i].charAt(0).toLocaleLowerCase());
-    levelArray[i] = levelArray[i].substring(1);
-    textContent += levelArray[i];
-  }
-
-  const textWidth = ctx.measureText(textContent).width;
-  textContent = '';
-  let trueTextPosX: number;
-  if (textAlign === 'center')
-    trueTextPosX = textPosX - textWidth / 2;
-  else
-    trueTextPosX = textPosX;
-  ctx.textAlign = 'left';
-
+  const startingTextAlign = ctx.textAlign;
   const startingFont = ctx.font;
   const startingFillStyle = ctx.fillStyle;
 
+  ctx.textAlign = 'left';
+  if (textAlign === 'center') {
+    const textWidth = ctx.measureText(removeColorCodes(text)).width;
+    textPosX = textPosX - textWidth / 2;
+  }
+
+  let textCompleted = ''; // The text that has been filled so far
   for (let i = 0; i < levelArray.length; i++) {
-    const fillStyle = colors[orderedColors[i]];
+    let currentColor = levelArray[i].charAt(0).toLocaleLowerCase();
+    levelArray[i] = levelArray[i].substring(1);
+
+    const fillStyle = colors[currentColor];
     let boldCompensate = false;
 
     switch (fillStyle) {
@@ -60,12 +55,13 @@ export default function (stringWithColorCodes: string, ctx: CanvasRenderingConte
     }
 
     ctx.fillText(levelArray[i],
-      trueTextPosX + ctx.measureText(textContent).width - (boldCompensate ?
-        textContent.length : 0), textPosY);
+      textPosX + ctx.measureText(textCompleted).width - (boldCompensate ?
+        textCompleted.length : 0), textPosY);
 
-    textContent += levelArray[i];
+    textCompleted += levelArray[i];
   }
 
+  ctx.textAlign = startingTextAlign;
   ctx.font = startingFont;
   ctx.fillStyle = startingFillStyle;
 }
