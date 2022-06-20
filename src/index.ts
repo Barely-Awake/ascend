9e6dedbfa736fab7d480bda28cd57e9484b3c961
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-06-19 19:36:00 -0700

    change(bot): Added new intents

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,8 @@ intents.add(
   Intents.FLAGS.GUILD_MEMBERS,
   Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
   Intents.FLAGS.DIRECT_MESSAGES,
+  Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+  Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
 );
 
 const client = new Client({
