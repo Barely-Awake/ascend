f9c8dafc618d85f1b5bf43c8097f52dd076266d9
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-06-19 19:36:22 -0700

    feat(utils): Added convertMode.ts

---
+++ src/utils/canvas/convertMode.ts
@@ -0,0 +1,16 @@
+export default function convertMode(mode: 'Overall' | 'Solo' | 'Doubles' | 'Threes' | 'Fours' | '4v4') {
+  switch (mode) {
+    case 'Overall':
+      return 'overAll';
+    case 'Solo':
+      return 'eight_one';
+    case 'Doubles':
+      return 'eight_two';
+    case 'Threes':
+      return 'four_three';
+    case 'Fours':
+      return 'four_four';
+    case '4v4':
+      return 'two_four';
+  }
+}
