7343d3d8fd5bc5623b7473e354d81547d3ce8c4e
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-07-05 14:04:22 -0700

    feat(commands): Made command handler work with custom prefixes!

--- src/bot/events/messageCreate.ts
+++ src/bot/events/messageCreate.ts
@@ -1,24 +1,27 @@
 import { Message } from 'discord.js';
 import config from '../../utils/misc/readConfig.js';
 import error from '../responses/error.js';
+import GuildData from '../../mongo/guildData.js';
 
 export default function (message: Message): void {
   commandHandler(message);
 }
 
-function commandHandler(message: Message) {
-  if (!message.content.startsWith(config.prefix))
+async function commandHandler(message: Message) {
+  const guildPrefix = await fetchGuildPrefix(message);
+
+  if (!message.content.startsWith(guildPrefix))
     return;
 
-  const messageArray = message.content.split(' ');
+  const messageContent = message.content.slice(guildPrefix.length);
+  const messageArray = messageContent.split(' ');
 
-  let commandName = messageArray[0].toLowerCase();
-  commandName = commandName.slice(config.prefix.length);
+  const commandName = messageArray[0].toLowerCase();
   const args = messageArray.slice(1);
 
   const command = message.client.commands.get(commandName);
 
-  if (!command || typeof command !== 'function')
+  if (typeof command !== 'function')
     return;
 
   try {
@@ -32,6 +35,26 @@ function commandHandler(message: Message) {
     );
     console.error(`An unknown error occurred with the command: ${commandName}. Error:\n`, err);
   }
+}
+
+async function fetchGuildPrefix(message: Message) {
+  const botMention = (message.client.user || '').toString();
+
+  if (message.content.startsWith(`${botMention} `))
+    return `${botMention} `;
+
+  if (message.content.startsWith(botMention))
+    return botMention;
+
+  if (!message.guild)
+    return config.prefix;
+
+  const fetchedData = await GuildData.find({serverId: message.guild.id});
+
+  if (fetchedData.length === 0)
+    return config.prefix;
+
+  return fetchedData[0].prefix;
 }
 
 export const settings = {
