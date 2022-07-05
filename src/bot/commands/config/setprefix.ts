210664573ac1bc6b6b94d3bb64f2751f06f81ae4
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-07-05 14:03:47 -0700

    feat(commands): Added setprefix.ts

---
+++ src/bot/commands/config/setprefix.ts
@@ -0,0 +1,38 @@
+import { Message } from 'discord.js';
+import { DescriptionTypes } from '../_example.js';
+import error from '../../responses/error.js';
+import GuildData from '../../../mongo/guildData.js';
+import config from '../../../utils/misc/readConfig.js';
+
+export default async function (message: Message, args: string[]) {
+  if (!message.guild || !message.member)
+    return error('This command has to be run in a guild', description.name, message);
+
+  if (!args || args[0] === undefined)
+    return error('You have to provide a prefix', description.name, message);
+
+  if (!message.member.permissions.has('MANAGE_GUILD'))
+    return error('You must have permission to manage the server to do that', description.name, message);
+
+  const prefix = args.join(' ');
+
+  let fetchedData = await GuildData.find({serverId: message.guild.id});
+
+  let guildInfo;
+  if (fetchedData.length === 0)
+    guildInfo = new GuildData({serverId: message.guild.id, prefix: config.prefix});
+  else
+    guildInfo = fetchedData[0];
+
+  guildInfo.prefix = prefix;
+  guildInfo.save();
+
+  message.channel.send(`Successfully set prefix to ${prefix}`);
+}
+
+export const description: DescriptionTypes = {
+  name: 'setprefix',
+  category: 'config',
+  description: 'Sets prefix in current guild',
+  usage: '<prefix>',
+};
