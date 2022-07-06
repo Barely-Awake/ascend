85f9ecdb30aee01a2456912035d6f5caa7075915
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-07-06 01:00:15 -0700

    feat(commands): Added muterole.ts

--- src/mongo/guildData.ts
+++ src/mongo/guildData.ts
@@ -6,12 +6,14 @@ interface IGuild {
   serverId: string;
 
   prefix: string;
+  muteRole: string | null;
 }
 
 const GuildSchema = new Schema<IGuild>({
   serverId: String,
 
   prefix: String,
+  muteRole: String,
 });
 
 const GuildData = model<IGuild>('Guild', GuildSchema);
