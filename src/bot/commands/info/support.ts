5a78e08d31b4096bff1ee3ab9576a0fad9bbf7f4
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-07-11 11:18:55 -0700

    feat(commands): Added support.ts

---
+++ src/bot/commands/info/support.ts
@@ -0,0 +1,13 @@
+import { Message } from 'discord.js';
+import { DescriptionTypes } from '../_example.js';
+
+export default function (message: Message, _: string[]) {
+  message.channel.send('For support please join discord.gg/PpdbKXKgT3');
+}
+
+export const description: DescriptionTypes = {
+  name: 'support',
+  category: 'info',
+  description: 'Provides the support discord invite link',
+  usage: '',
+};
