884caaf972ce5039ff6d8dcec4aa073d54b1c9db
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-07-11 11:26:24 -0700

    change(types): Merged nameHistory.ts & playerUuid.ts into mojangApiTypes.ts

---
+++ src/types/mojangApiTypes.ts
@@ -0,0 +1,11 @@
+export type NameHistory = MojangNameHistoryIndex[]
+
+interface MojangNameHistoryIndex {
+  name: string;
+  changedToAt?: number;
+}
+
+export interface PlayerUuid {
+  name: string;
+  id: string;
+}
