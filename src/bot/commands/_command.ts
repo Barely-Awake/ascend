d7d3897a67e317914ecc63759b75afec58d071b5
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-08-02 13:00:04 -0700

    change(commands): Updated template (_command.ts)

--- src/bot/commands/_command.ts
+++ src/bot/commands/_command.ts
@@ -1,21 +1,29 @@
 // Template command
 import { Message } from 'discord.js';
+import { CommandCategory, CommandClass } from '../../types/discord.js';
 
-export default function (message: Message, args: string[]) {
-  return;
-}
+export default class _Example implements CommandClass {
+  public name: string;
+  public category: CommandCategory;
+  public aliases: string[] | null;
+  public description: string;
+  public usage: string;
 
-export const commandInfo: CommandInfo = {
-  name: '_example',
-  category: 'info',
-  description: 'Example command',
-  usage: '<Required Argument> [Optional Argument]',
-};
+  constructor(
+    name = '_example',
+    category: CommandCategory = 'info',
+    aliases: string[] | null = ['example_'], // Set to null for no aliases
+    description = 'Example command',
+    usage = '<Required Argument> [Optional Argument]',
+  ) {
+    this.name = name;
+    this.category = category;
+    this.aliases = aliases;
+    this.description = description;
+    this.usage = usage;
+  }
 
-export interface CommandInfo {
-  name: string;
-  category: 'config' | 'info' | 'moderation' | 'minecraft' | 'externalApis' | 'misc';
-  aliases?: string[];
-  description: string;
-  usage: string;
+  command(message: Message, args: string[]) {
+    return;
+  }
 }
