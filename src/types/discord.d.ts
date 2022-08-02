fa934243ced73b07e6ed56211e2fc6f8b99d722b
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-08-02 12:44:34 -0700

    change(types): Updated types for class based commands

--- src/types/discord.d.ts
+++ src/types/discord.d.ts
@@ -1,20 +1,27 @@
 import { Collection, Message } from 'discord.js';
-import { CommandInfo } from '../bot/commands/_command.js';
 
 // Without this file, assigning client.commands to anything would make typescript throw an error
 declare module 'discord.js' {
   export interface Client {
-    commands: Collection<string, CommandFunction>;
+    commands: CommandCollection;
     cache: Cache;
   }
 }
 
-export type CommandFunction = (message: Message, args: string[]) => void | Promise<void>
+export interface CommandClass {
+  name: string;
+  category: CommandCategory;
+  aliases: string[] | null;
+  description: string;
+  usage: string;
+  command: CommandFunction;
+}
+
+export type CommandFunction = (message: Message, args: string[]) => any;
+export type CommandCollection = Collection<string, CommandClass>;
+export type CommandCategory = 'config' | 'info' | 'moderation' | 'minecraft' | 'externalApis' | 'misc';
 
 interface Cache {
-  commandInfo: {
-    [command: string]: CommandInfo
-  };
   prefixes: {
     [serverId: string]: string
   };
