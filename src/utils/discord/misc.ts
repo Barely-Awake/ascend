8fabf9b2c59533a730f2c53b87d156d98abd1472
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-08-03 00:42:13 -0700

    change(utils): Removed checkPermissions function in favor of decorator

--- src/utils/discord/misc.ts
+++ src/utils/discord/misc.ts
@@ -1,18 +1,9 @@
-import { GuildMember, PermissionResolvable } from 'discord.js';
+import { GuildMember } from 'discord.js';
 
 type ValidTimeStampStyles = 't' | 'T' | 'd' | 'D' | 'f' | 'F' | 'R'
 
 export function messageTimeStamp(timeStamp: number, style: ValidTimeStampStyles = 'f'): string {
   return `<t:${timeStamp}:${style}>`;
-}
-
-export function checkPermissions(user: GuildMember, ...permissions: PermissionResolvable[]) {
-  permissions.forEach((v) => {
-    if (!user.permissions.has(v))
-      return false;
-  });
-
-  return true;
 }
 
 export function canModerateUser(user: GuildMember, target: GuildMember, guildOwnerId: string) {
