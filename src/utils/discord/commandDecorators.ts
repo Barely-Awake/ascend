f6b8e6cdd93b78db2cb7f98335d42c9421d7b6c3
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-08-20 13:37:05 -0700

    fix(utils): Fixed requireBotPermissions command decorator

--- src/utils/discord/commandDecorators.ts
+++ src/utils/discord/commandDecorators.ts
@@ -1,11 +1,11 @@
-import { PermissionResolvable } from 'discord.js';
+import { Message, PermissionResolvable } from 'discord.js';
 
 export function requirePermission(permission: PermissionResolvable) {
   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
     const originalFunction = descriptor.value;
     descriptor.value = function () {
-      const message = arguments[0];
-      if (!message.member.permissions.has(permission))
+      const message: Message = arguments[0];
+      if (!message.member!.permissions.has(permission))
         return message.reply('You don\'t have the required permissions');
 
       return originalFunction.apply(this, arguments);
@@ -16,9 +16,10 @@ export function requirePermission(permission: PermissionResolvable) {
 export function requireBotPermission(permission: PermissionResolvable) {
   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
     const originalFunction = descriptor.value;
-    descriptor.value = function () {
-      const message = arguments[0];
-      if (!message.guild.me.permissions.has(permission))
+    descriptor.value = async function () {
+      const message: Message = arguments[0];
+      const botMember = await message.guild!.members.fetchMe();
+      if (!botMember.permissions.has(permission))
         return message.reply('I don\'t have the required permissions' +
           '(*I\'m a moderation bot, it\'s recommended to give me admin*)');
 
@@ -31,7 +32,7 @@ export function onlyInGuild() {
   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
     const originalFunction = descriptor.value;
     descriptor.value = function () {
-      const message = arguments[0];
+      const message: Message = arguments[0];
       if (!message.guild || !message.member)
         return message.reply('This command must be run in a guild');
 
@@ -44,8 +45,8 @@ export function requireArgs(argNumber: number) {
   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
     const originalFunction = descriptor.value;
     descriptor.value = function () {
-      const message = arguments[0];
-      const args = arguments[1];
+      const message: Message = arguments[0];
+      const args: string[] = arguments[1];
       if (args.length < argNumber)
         return message.reply('You must provide enough arguments');
 
