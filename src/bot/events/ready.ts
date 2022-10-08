010630e22ee3f019b1ea77001791a200d768c93f
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-10-07 22:39:39 -0700

    change(events): Updated ready event message

--- src/bot/events/ready.ts
+++ src/bot/events/ready.ts
@@ -2,7 +2,7 @@ import { Client } from 'discord.js';
 import { Settings } from './_event.js';
 
 export default function (client: Client) {
-  console.log(`Logged in as ${client.user?.tag}`);
+  console.log(`Client ready, logged in as ${client.user?.tag}!`);
 }
 
 export const settings: Settings = {
