8394e7ee2fcde1c626b3249a2d393a19373a32e9
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-10-17 22:46:54 -0700

    feat(utils): Updated makeWebRequest.ts to comply with Antisniper's terms

--- src/utils/misc/makeWebRequest.ts
+++ src/utils/misc/makeWebRequest.ts
@@ -1,8 +1,16 @@
-import fetch from 'node-fetch';
+import fetch, { Headers } from 'node-fetch';
 
 export default async function makeWebRequest(url: string) {
+  const requestHeaders = new Headers({
+    'User-Agent': 'ascend',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  });
+
   try {
-    const request = await fetch(url);
+    const request = await fetch(url, {
+      headers: requestHeaders,
+    });
     const data = await request.json();
 
     return data || null;
