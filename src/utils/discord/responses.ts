c04c1cdd89eb2f3f98d86b5702f49d1d8dc266b1
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-11-12 21:09:41 -0800

    change(utils): remove footer from error response

--- src/utils/discord/responses.ts
+++ src/utils/discord/responses.ts
@@ -1,14 +1,10 @@
-import config from '../misc/readConfig.js';
 import { EmbedBuilder, Message } from 'discord.js';
 
 export function error(description: string, message: Message) {
   const embed = new EmbedBuilder()
-    .setTitle("Sorry, I couldn't finish that command.")
+    .setTitle("Sorry, I couldn't finish that command!")
     .setColor('#ff0000')
-    .setDescription(description)
-    .setFooter({
-      text: `Sent by ${config.botName}`,
-    });
+    .setDescription(description);
 
   return message.reply({ embeds: [embed] });
 }
